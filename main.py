8b9e3e6bf950dbc7e91d4290d9df0fa5c0f2fee6
dumitru.vlad1996 <dumitru.vlad1996@gmail.com>
2021-06-17 17:47:40 +0300

    handle /start command

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def results_command(update, context):
     report = fileHandlerModule.getReport()
     update.message.reply_text(report)
 
+def start_command(update, context):
+    update.message.reply_text("Salut, foloseste comanda /rezultate pentru a vedea ultima extragere de la Loteria Romana")
+    update.message.reply_text("Ca sa folosesti comanda, doar scrie-mi /rezultate sau apasa pe butonul [/] din dreapta campului de scris")
+    update.message.reply_text("Pentru diverse erori/comentarii : dumitru.vlad1996@gmail.com")
+    checkContact(update)
+
 def checkContact(update):
     contact = str(update.message.chat_id)
     contactList = fileHandlerModule.getContactsList()
@@ -30,7 +36,9 @@ def cron_job(context):
             context.bot.send_message(contact, report)
 
 def main():
+    fileHandlerModule.initializeFiles()
     dp.add_handler(CommandHandler("rezultate", results_command))
+    dp.add_handler(CommandHandler("start", start_command))
     job_queue.run_repeating(cron_job, interval=300, first=1.0)
     updater.start_polling()
     updater.idle()
